4dc14f299b46ba2507545b81db22253bc2bfe5ab
Phil Chrapka <pchrapka@gmail.com>
2011-07-05 15:40:40 -0400

    Removed a few comments

--- genTrainingDataMLPEKF.m
+++ genTrainingDataMLPEKF.m
@@ -5,18 +5,11 @@ function [ points, groupName ] = genTrainingDataMLPEKF( epochs, samples )
 numElements = 2;
 
 points = zeros(epochs,numElements,samples);
-% groups = zeros(epochs,samples);
 groupName = cell(epochs,samples);
-% desiredOutput = zeros(epochs,samples);
 
 for k=1:epochs
     % Generate the training and test data
     [points(k,:,:), groupName(k,:)] = genTrainingData(samples);
-    % Differentiate the groups
-%     groups(k,:) = ismember(groupName(k,:),'red');
-    
-    % Make red = 0.7 and black = -0.7
-%     desiredOutput(k,:) = groups(k,:)*0.7 + (groups(k,:)-1)*0.7;
 end
 
 end
