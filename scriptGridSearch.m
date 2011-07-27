73b0b198a62e5366ad5ef9a276141bdb4529dfd4
Phil Chrapka <pchrapka@gmail.com>
2011-07-27 16:22:02 -0400

    Modified scriptGridSearch to save the weights as well

--- scriptGridSearch.m
+++ scriptGridSearch.m
@@ -12,7 +12,7 @@ epsilon = linspace(0.001,0.01,samples);
 % q = linspace(1e-6,0.1,samples);
 % HN = round(linspace(5,1000,samples));
 q = 0.001;
-HN = 10:10:100;
+HN = 10;%10:10:100;
 annealingFlag = true;
 perfPlotFlag = false;
 
@@ -20,13 +20,18 @@ saveFolder = 'C:\Users\Phil\Documents\School\Masters\ECE 739 - Neural Networks\P
 
 %% Perform the search
 perf = zeros(length(HN),length(eta),length(epsilon),epochs);
+% Initialize a cell array for weight vectors
+w = cell(length(HN),1);
 finalAvgPerf = zeros(length(HN),length(eta),length(epsilon));
 for i=1:length(HN)
+    numElements = 2;
+    numWeights = HN(i)*(numElements+2) + 1;
+    w{i} = zeros(length(eta),length(epsilon),numWeights);
     for j=1:length(eta)
         for k=1:length(epsilon)
             disp(['HN: ' num2str(HN(i)) ' eta: ' num2str(eta(j)) ' epsilon: ' num2str(epsilon(k))]);
             % Run the algorithm
-            [perf(i,j,k,:),~] = runEKFMLP2(...
+            [perf(i,j,k,:),w{i}(j,k,:)] = runEKFMLP2(...
                 epochs,testSamples,...
                 HN(i), eta(j), epsilon(k), q, ...
                 annealingFlag, perfPlotFlag);
