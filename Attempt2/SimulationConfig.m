ff442297f778a35dc5e6a96b5ba34245808bc68c
Phil Chrapka <pchrapka@gmail.com>
2011-08-18 11:32:12 -0400

    Adding SimulationConfig since some of the older data sets use this class...bummer

---
+++ Attempt2/SimulationConfig.m
@@ -0,0 +1,51 @@
+classdef SimulationConfig
+    %SIMULATIONCONFIG Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        % Number of epochs to run the simulation
+        NumEpochs = 500;
+        % Number of data samples per epoch
+        NumPointsPerEpoch = 200;
+        % Desired output value (Red is +ve, Black is -ve)
+        DesiredOutputValue = 0;
+        
+        % Initial value for P
+        p = 0;
+        % Value for Q
+        q = 0;
+        DoAnnealing = false;
+        % Value for R
+        r = 0;
+        
+        % MLP Configuration
+        MLPConfigObj
+        
+        % Number of test points used
+        NumTestPoints = 200;
+        % Flag for plotting performance during the running simulation
+        PlotPerf = false;
+        % Flag for plotting the classified data
+        PlotClassifiedData = false;
+        PlotClassifiedDataFigHandle;
+        
+        % Algorithm output
+        FinalW = [];
+        FinalP = [];
+        Exception = [];
+        % Array of performance values for each epoch
+        Performance
+    end
+    
+    methods
+        function obj = SimulationConfig()
+        end
+        
+        function obj = Init(obj)
+            % Allocate memory before we use it
+            obj.Performance = zeros(obj.NumEpochs,1);
+        end
+    end
+    
+end
+
